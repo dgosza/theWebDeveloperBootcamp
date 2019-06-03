f689212aacca8f15287f7ed9a21dbb4d58b93665
Diego Souza <diegomagasouza@gmail.com>
2019-06-03 15:30:56 -0300

    javascript files

---
+++ Section9Javascript/script.js
@@ -0,0 +1,6 @@
+var userName = prompt("what's your first name ?");
+var lastName = prompt("what's your last name ?");
+var agePerson = prompt ("last, what's your age ?");
+	
+console.log(userName + " " + lastName);
+console.log(agePerson);
